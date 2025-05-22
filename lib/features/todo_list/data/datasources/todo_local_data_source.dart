d23b5dc513b78bf3a789db3535464d46adb22466
Victor River <victor@devx.com.au>
2025-05-22 21:48:44 +0700

    fix: clean code

--- lib/features/todo_list/data/datasources/todo_local_data_source.dart
+++ lib/features/todo_list/data/datasources/todo_local_data_source.dart
@@ -8,13 +8,23 @@ import 'package:todo/features/todo_list/data/models/todo_model.dart';
 class TodoLocalDataSource {
   const TodoLocalDataSource(this._todoBox);
 
-  final CollectionBox<Map<String, dynamic>> _todoBox;
+  final CollectionBox<List<Map<String, dynamic>>> _todoBox;
 
   ///Get the todo list from local database
   Future<List<TodoModel>> getTodos() async {
     final todos = await _todoBox.get(AppConstants.userTodosKey);
-    final listMap = (todos as List).cast<Map<String, dynamic>>();
-    return listMap.map((todo) => TodoModel.fromJson(todo)).toList();
+    return todos?.map((todo) => TodoModel.fromJson(todo)).toList() ?? [];
   }
 
+  ///Add new todo to the local database
+  Future<TodoModel> addTodo(TodoModel todo) async {
+    final currentTodoList = await _todoBox.get(AppConstants.userTodosKey) ?? [];
+    currentTodoList.add(todo.toJson());
+    await _todoBox.put(AppConstants.userTodosKey, currentTodoList);
+    return todo;
+  }
+
+
+
+  
 }
