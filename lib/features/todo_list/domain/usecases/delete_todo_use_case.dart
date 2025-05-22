87087ced5f47d4d3d68cae7f72c2c14cec06fbde
Victor River <victor@devx.com.au>
2025-05-22 23:26:49 +0700

    refactor: clean code

--- lib/features/todo_list/domain/usecases/delete_todo_use_case.dart
+++ lib/features/todo_list/domain/usecases/delete_todo_use_case.dart
@@ -1,6 +1,5 @@
 import 'package:injectable/injectable.dart';
 import 'package:todo/features/todo_list/data/repositories/todo_repository.dart';
-import 'package:todo/features/todo_list/domain/entities/todo_entity.dart';
 
 @injectable
 class DeleteTodoUseCase {
